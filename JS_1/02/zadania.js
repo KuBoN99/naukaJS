69a7e0bdab59a388abc4b53b1045f5d06084dedb
KuBoN99 <mazx89@gmail.com>
2017-10-28 12:22:10 +0200

    lesson2

---
+++ JS_1/02/zadania.js
@@ -0,0 +1,39 @@
+// Zadanie 1
+var ex1 = (5 == 8);                   //Wynik przykładu 1
+var ex2 = (12 == -1);                 //Wynik przykładu 2
+var ex3 = (2 !== 2);                  //Wynik przykładu 3
+var ex4 = (999 == 0.234);             //Wynik przykładu 4
+
+//zadanie 2
+var ex5 = (5 !== 8);                  //Wynik przykładu 1
+var ex6 = (12 !== -1);                //Wynik przykładu 2
+var ex7 = (2 !== 2.01);               //Wynik przykładu 3
+var ex8 = (999 !== 0.234);            //Wynik przykładu 4
+
+// Zadanie 3
+var ex9 = ((3 == 4) && (1 == 7));        //Wynik przykładu 1
+var ex10 = ((12 == 5) || (6 == -4));      //Wynik przykładu 2
+var ex11 = !(12 !== 5);             //Wynik przykładu 3
+
+// Zadanie 4
+var ex12 = ((2 <= 8) && (1 == 1));        //Wynik przykładu 1
+var ex13 = ((0 == 3) || !(13 == -2));   //Wynik przykładu 2
+var ex14 = !(!(12 !== 5));          //Wynik przykładu 3
+
+
+
+// Nie zmieniaj kodu poniżej
+document.getElementById("ex1").innerText += ex1;
+document.getElementById("ex2").innerText += ex2;
+document.getElementById("ex3").innerText += ex3;
+document.getElementById("ex4").innerText += ex4;
+document.getElementById("ex5").innerText += ex5;
+document.getElementById("ex6").innerText += ex6;
+document.getElementById("ex7").innerText += ex7;
+document.getElementById("ex8").innerText += ex8;
+document.getElementById("ex9").innerText += ex9;
+document.getElementById("ex10").innerText += ex10;
+document.getElementById("ex11").innerText += ex11;
+document.getElementById("ex12").innerText += ex12;
+document.getElementById("ex13").innerText += ex13;
+document.getElementById("ex14").innerText += ex14;
